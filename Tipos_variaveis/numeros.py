6495ac861b827287bb317ca4dfaa67acbd737524
Ezebit0 <edumlordelo@gmail.com>
2025-12-27 23:27:18 -0300

    add basic numeric types

---
+++ Tipos_variaveis/numeros.py
@@ -0,0 +1,43 @@
+# Inteiro 
+numero_inteiro = 4
+print("Inteiro =", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante =", numero_real)
+
+# Equações
+# soma + 
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+# subtração -
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# multiplicação *
+multiplicacao = 2 * 2
+print("2 * 2 =", multiplicacao)
+
+# divisão /
+divisao = 5 / 2
+print("5 / 2 =", divisao)
+print("Tipo de variavel do resultado da divisao =", type(divisao))
+
+# divisão inteiro //
+divisao = 5 // 2
+print("5 // 2 =", divisao)
+print("Tipo de variavel do resultado da divisao =", type(divisao))
+
+# modulo %
+modulo = 5 % 2
+print("Modulo =", modulo)
+
+# type()
+print("Tipo de variavel inteiro=", type(numero_inteiro))
+print("Real =", type(numero_real))
+
+# int()
+print("Valor em inteiro=", int(divisao))
+# float()
+print("Valor em float=", float(divisao))

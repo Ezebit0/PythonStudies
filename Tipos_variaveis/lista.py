84bbf773555b5386de80d2eda8b1e4b28f608468
Ezebit0 <edumlordelo@gmail.com>
2026-01-03 17:14:24 -0300

    add basic list concepts

---
+++ Tipos_variaveis/lista.py
@@ -0,0 +1,15 @@
+# Coleção de elementos ordenaveis e mutaveis(permite que mude o valor)
+# Declaração
+
+minha_lista = [1, 2, 3, 4, 5, "Eduardo", True, False]
+
+# Exibindo a lista
+print("Minha lista de exemplo", minha_lista)
+
+# Exibindo a lista
+minha_lista[0] = "python"
+print("minha_lista[0]:", minha_lista[0]) # elemento 0
+print("minha_lista[0]:", minha_lista[5]) # elemento 5
+print("minha_lista[1:7]:", minha_lista[1:7]) # elemento de 1 a 6 (7)
+print("minha_lista[:6]:", minha_lista[:6]) # todos elementos antes de 6
+print("minha_lista[3:]:", minha_lista[3:]) # todos elementos após o 3

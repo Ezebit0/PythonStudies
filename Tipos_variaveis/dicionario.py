e26a8e7712c62cc96fd4d6b9246518458302dc64
Ezebit0 <edumlordelo@gmail.com>
2026-01-04 20:06:46 -0300

    add dictionary basics

---
+++ Tipos_variaveis/dicionario.py
@@ -0,0 +1,13 @@
+# Criando um dicionario de exemplo
+
+pessoa = {"nome": "Eduardo", "idade": "18", "cidade": "Osasco"}
+
+# Exibindo o dicionario completo            
+print("Meu dicionario de exemplo:", pessoa)
+# Acessando valores por chaves
+print("Nome:", pessoa ["nome"])
+print("Idade:", pessoa ["idade"])
+print("Cidade:", pessoa ["cidade"])
+
+pessoa["sobrenome"] = "Mendes"
+print("Sobrenome:", pessoa ["sobrenome"])

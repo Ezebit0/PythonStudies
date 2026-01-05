304b7f768af926d9ac2191003a0fa422260b3127
Ezebit0 <edumlordelo@gmail.com>
2026-01-05 18:28:35 -0300

    improve task visualization

--- projeto_de_estudo/gerenciador.py
+++ projeto_de_estudo/gerenciador.py
@@ -2,10 +2,17 @@ def adicionar_tarefa(tarefas, nome_tarefa):
 
     # Tarefa: Nome da tarefa
     # Completada: Indicar se essa tarefa foi completada ou não
-    tarefa = {"tarefa:": nome_tarefa, "completada": False}
-    tarefas.append(tareaf)
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
     return
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✓" if tarefa["completada"] else "✗"
+        nome_tarefa = tarefa["tarefa"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
 
 tarefas = []
 
@@ -13,23 +20,22 @@ tarefas = []
 while True:
     print("\nMenu do gerenciador de lista de tarefas:")
     print("1. Adicionar tarefa")
-
-    
     print("2. ver tarefa")
-
     print("3. Atualizar tarefa")
-
     print("4. Completar tarefa")
-
     print("5. Deletar tarefas completadas")
-
     print("6. Sair")
+
     escolha = input("Digite a sua escolha: ")
 
 
     if escolha == "1":
         nome_tarefa = input ("Digite o nome da tarefa que deseja adicionar:")
         adicionar_tarefa(tarefas, nome_tarefa)
+
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+
     elif escolha == "6":
         break
 

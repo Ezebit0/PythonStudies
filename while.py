f556397cbd3e30af055ab8b2ed297a42fc0db7dd
Ezebit0 <edumlordelo@gmail.com>
2026-01-05 17:31:22 -0300

    add while basics

---
+++ while.py
@@ -0,0 +1,11 @@
+# while é o tipo de looping utilizado para repetir o bloco de codigo enquanto a condicao for verdadeira
+
+print("Exemplo de while")
+contador = 0
+while contador < 6:
+    print("Contagem:", contador)
+    contador += 1
+    if contador == 6:
+        break
+
+print("Programa finalizado")

be4e20dff034e768e5789136246ca1cdc176d149
Ezebit0 <edumlordelo@gmail.com>
2026-01-05 15:18:52 -0300

    add conditional fundamentals

---
+++ condicionais.py
@@ -0,0 +1,25 @@
+# if, elif, else
+
+# Exemplo de "if"
+
+idade = 18
+print("Exemplo de comando if")
+if idade >= 18: # Maior ou igual
+    print("Voce é maior de idade")
+elif idade >= 12:
+    print("Você é um adolescente")
+else: # Exemplo de "else"
+    print("Voce não é maior de idade")
+
+# Exemplão
+mensagem = "Pode tirar a CNH" if idade >= 18 else "Voce não pode tirar a CNH"
+
+if idade == 19: # Igual á
+    print("Voce tem 19 anos")
+
+if idade != 10: # Diferente
+    print("Voce não tem 10 anos")
+
+
+
+
